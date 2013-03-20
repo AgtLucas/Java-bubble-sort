c27d1efb05657432a19c8932ecd1d94ffa2c2101
Lucas <metallucas.ds@gmail.com>
2013-03-19 22:56:51 -0300

    Adcionado código corrigido na aula

--- src/aula02/bubble/sort/Aula02BubbleSort.java
+++ src/aula02/bubble/sort/Aula02BubbleSort.java
@@ -6,19 +6,18 @@ import java.util.Scanner;
  *
  * @author AgtLucas
  */
-class Funcionario {
-    String name;
-    float salario;
-}
 
 public class Aula02BubbleSort {
+    public static class Funcionario {
+        public String name;
+        public float salario;
+    }
 
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int op, n, i, troca;
-        int x[] = new int[10];
         Funcionario[] funcionario = new Funcionario[5];
-        Funcionario aux = new Funcionario();
+        Funcionario aux;
+        int op, n, i, troca;
         
         do {
             System.out.println("Menu");
@@ -44,9 +43,9 @@ public class Aula02BubbleSort {
                     n = 1;
                     troca = 1;
                     
-                    while(n <= 5 && troca == 1) {
+                    while(n <= funcionario.length && troca == 1) {
                         troca = 0;
-                        for(i = 0; i <= 3; i++) {
+                        for(i = 0; i <= (funcionario.length - 2); i++) {
                             if(funcionario[i].salario > funcionario[i + i].salario) {
                                 troca = 1;
                                 aux = funcionario[i];
@@ -57,23 +56,54 @@ public class Aula02BubbleSort {
                         n = n + 1;
                     }  
                     
-                    for(i = 0; i <= 9; i++) {
+                    for(i = 0; i <= funcionario.length - 1; i ++) {
                         System.out.println(funcionario[i].salario);
                     }
                 }
+                if (op == 3) {
+                    n = 1;
+                    troca = 1;
+                    
+                    while(n <= funcionario.length && troca == 1) {
+                        troca = 0;
+                        for(i = 0; i <= (funcionario.length - 2); i++) {
+                            if(funcionario[i].salario < funcionario[i + 1].salario) {
+                                troca = 1;
+                                aux = funcionario[i];
+                                funcionario[i] = funcionario[i + 1];
+                                funcionario[i + 1] = aux;
+                            }
+                        }
+                        n = n + 1;
+                    }  
+                    
+                    for(i = 0; i <= funcionario.length - 1; i++) {
+                        System.out.println(funcionario[i].salario);
+                    }
+                }
+                if (op == 4) {
+                    n = 1;
+                    troca = 1;
+                    
+                    while(n <= funcionario.length && troca == 1) {
+                        troca = 0;
+                        for(i = 0; i <= (funcionario.length - 2); i++) {
+                            if (funcionario[i].name.compareToIgnoreCase(funcionario[i + 1].name) > 0) {
+                                troca = 1;
+                                aux = funcionario[i];
+                                funcionario[i] = funcionario[i + 1];
+                                funcionario[i + 1] = aux;
+                            }
+                        }
+                        n = n + 1;
+                    }
+                    
+                    for(i = 0; i <= funcionario.length - 1; i++) {
+                        System.out.println(funcionario[i].name);
+                    }
+                }
             }
-        } while (op != 4);
-        
-//        System.out.println("Nome");
-//        f1.name = in.next();
-//        System.out.println("Salario");
-//        f1.salario = in.nextFloat();
-//        
-//        System.out.println(f1.name);
-        
-//        int op;
-//        int x[] = new int[10];
-        
+        } while (op != 5);
         
     }
 }
